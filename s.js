5f535a1ded9a8a74f8beb6355b271e616f2f2fb9
Aquilespina <apinao2000@alumno.ipn.mx>
2023-09-10 00:24:19 -0600

    Data product

--- s.js
+++ s.js
@@ -1,26 +1,58 @@
-const selectButtons = document.querySelectorAll('.select-button');
-const selectedProductsList = document.getElementById('selected-products-list');
-const confirmButton = document.getElementById('confirm-button');
 
-const selectedProducts = [];
+// JavaScript
+var contadores = document.querySelectorAll(".contador");
+var sumadores = document.querySelectorAll(".mas");
+var restadores = document.querySelectorAll(".menos");
+var seleccionarBoton = document.getElementById("seleccionar");
+var formulario = document.getElementById("formulario");
+var valor1Input = document.getElementById("valor1");
+var valor2Input = document.getElementById("valor2");
 
-selectButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const productName = button.getAttribute('data-product');
-        selectedProducts.push(productName);
-        updateSelectedProductsList();
+var prevValues = [];
+
+function calcular(index) {
+    var value = contadores[index].value;
+    var isValid = /^[1-9][0-9]*$/.test(value);
+    if (!isValid) {
+        contadores[index].value = prevValues[index];
+    } else {
+        prevValues[index] = value;
+    }
+}
+
+seleccionarBoton.addEventListener("click", function() {
+    valor1Input.value = contadores[0].value;
+    valor2Input.value = contadores[1].value;
+    // Agrega más asignaciones de valores a campos del formulario según sea necesario
+});
+
+for (var i = 0; i < sumadores.length; i++) {
+    sumadores[i].addEventListener("click", function(event) {
+        var index = Array.from(sumadores).indexOf(event.target);
+        contadores[index].value = Number(contadores[index].value) + 1;
+        calcular(index);
     });
-});
+}
 
-confirmButton.addEventListener('click', () => {
-   
-});
+for (var i = 0; i < restadores.length; i++) {
+    restadores[i].addEventListener("click", function(event) {
+        var index = Array.from(restadores).indexOf(event.target);
+        contadores[index].value = Number(contadores[index].value) - 1;
+        calcular(index);
+    });
+}
 
-function updateSelectedProductsList() {
-    selectedProductsList.innerHTML = '';
-    selectedProducts.forEach(product => {
-        const li = document.createElement('li');
-        li.textContent = product;
-        selectedProductsList.appendChild(li);
+for (var i = 0; i < contadores.length; i++) {
+    contadores[i].addEventListener("change", function(event) {
+        var index = Array.from(contadores).indexOf(event.target);
+        calcular(index);
+    });
+
+    contadores[i].addEventListener("keyup", function(event) {
+        var index = Array.from(contadores).indexOf(event.target);
+        if (contadores[index].value === "") {
+            return;
+        }
+        calcular(index);
     });
 }
