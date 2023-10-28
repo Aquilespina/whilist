efa58f373de13aad42cd8d52dddc47c5b99031d0
Aquilespina <apinao2000@alumno.ipn.mx>
2023-10-28 13:14:50 -0600

    Php y js correcto

--- conexion.php
+++ conexion.php
@@ -1,16 +1,44 @@
 <?php
 
 
-$server = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db = 'boda';
-    $conexion = mysqli_connect($server, $user, $pass, $db);
+    $servername = "localhost";
+ $username = "id21388328_adm";
+ $password = "Producto01#";
+ $dbname = "id21388328_regalos";
+ 
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
 
-if($conexion){
-    echo 'Conectado exitosamente a la base de datos';   
-}else{  
-    echo 'No se pudo conectar a la base de datos';    
-}   
-?>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $numeroCelular = $_POST["numeroCelular"];
+    $nombreProducto = $_POST["nombreProducto"];
+    $valor = $_POST["valor"];
+
+    if (isset($nombreProducto, $valor)) {
+        $sql = "INSERT INTO pedidos (nombre, correo, numeroCelular,nombreProducto,valor) VALUES ($nombre, $correo, $numeroCelular,$nombreProducto,$valor)";
+        $stmt = $conn->prepare($sql);
+        
+        if ($stmt) {
+            $stmt->bind_param("ssssd", $nombre, $correo, $numeroCelular, $nombreProducto, $valor);
+            
+            if ($stmt->execute()) {
+                echo "Los datos se han guardado correctamente en la base de datos.";
+            } else {
+                die("Error al guardar los datos en la base de datos: " . $stmt->error);
+            }
+        } else {
+            die("Error en la preparación de la consulta: " . $conn->error);
+        }
+
+        $stmt->close();
+    } else {
+        echo "Uno o ambos campos no están configurados.";
+    }
+}
+
+$conn->close();
--- s.js
+++ s.js
@@ -1,18 +1,19 @@
-// JavaScript
-var contadores = document.querySelectorAll(".contador");
-var sumadores = document.querySelectorAll(".mas");
-var restadores = document.querySelectorAll(".menos");
-var selectButtons = document.querySelectorAll(".select-button");
-var formulario = document.getElementById("formulario");
-var infoSeleccion = document.getElementById("infoSeleccion");
-var nombreInput = document.getElementById("nombre");
-var correoInput = document.getElementById("correo");
-var productosSeleccionados = {};
-var prevValues = [];
+const contadores = document.querySelectorAll(".contador");
+const sumadores = document.querySelectorAll(".mas");
+const restadores = document.querySelectorAll(".menos");
+const selectButtons = document.querySelectorAll(".select-button");
+const formulario = document.getElementById("formulario");
+const infoSeleccion = document.getElementById("infoSeleccion");
+const nombreInput = document.getElementById("nombre");
+const correoInput = document.getElementById("correo");
+const numeroCelularInput = document.getElementById("numeroCelular");
+const nombreProductoInput = document.getElementById("nombreProducto"); // Agregado un input para el nombre del producto
+const productosSeleccionados = {};
+const prevValues = [];
 
 function calcular(index) {
-    var value = contadores[index].value;
-    var isValid = /^[0-9][0-9]*$/.test(value);
+    const value = contadores[index].value;
+    const isValid = /^[0-9][0-9]*$/.test(value);
     if (!isValid) {
         contadores[index].value = prevValues[index];
     } else {
@@ -20,109 +21,102 @@ function calcular(index) {
     }
 }
 
-function agregarProducto(numero, valor) {
-    var botonSeleccionado = selectButtons[numero - 1];
-    var nombreProducto = botonSeleccionado.getAttribute("data-product");
-    
+function agregarProducto(numero, valor, nombreProducto) {
     if (productosSeleccionados.hasOwnProperty(numero)) {
         productosSeleccionados[numero].valor = valor;
     } else {
         productosSeleccionados[numero] = { nombre: nombreProducto, valor: valor };
         mostrarInfoSeleccion(numero, valor, nombreProducto);
+        // Actualiza el campo oculto con el valor de nombreProducto
+        nombreProductoInput.value = nombreProducto;
     }
     actualizarFormulario();
 }
 
-
 function mostrarInfoSeleccion(numero, valor, nombreProducto) {
-    var infoDiv = document.createElement("div");
+    const infoDiv = document.createElement("div");
 
     infoSeleccion.appendChild(infoDiv);
 }
 
 function actualizarFormulario() {
-    formulario.style.display = "block"; // Mostrar el formulario después de seleccionar productos
-    nombreInput.focus(); // Colocar el foco en el campo de nombre
-
-    // Limpia el formulario
+    formulario.style.display = "block";
+    nombreInput.focus();
     formulario.innerHTML = "";
 
-    // Agrega campos para los productos seleccionados
-    for (var numero in productosSeleccionados) {
-        var producto = productosSeleccionados[numero];
-
-        // Agrega la información del producto
-        var infoDiv = document.createElement("div");
-        infoDiv.textContent =  producto.nombre ;
+    for (const numero in productosSeleccionados) {
+        const producto = productosSeleccionados[numero];
+        const infoDiv = document.createElement("div");
+        infoDiv.textContent = `${producto.nombre}: ${producto.valor}`;
         formulario.appendChild(infoDiv);
-
-        // Agrega el valor del producto
-        var nuevoCampo = document.createElement("input");
-        nuevoCampo.type = "text";
-        nuevoCampo.value = producto.valor;
-        nuevoCampo.readOnly = true;
-        formulario.appendChild(nuevoCampo);
     }
 
-    // Agrega campos para el nombre y el correo electrónico
+    const br = document.createElement("br");
 
-    formulario.appendChild(document.createElement("br"));
+    formulario.appendChild(br);
     formulario.appendChild(document.createTextNode("Nombre: "));
     formulario.appendChild(nombreInput);
-    formulario.appendChild(document.createElement("br"));
+
+    formulario.appendChild(br);
     formulario.appendChild(document.createTextNode("Correo Electrónico: "));
     formulario.appendChild(correoInput);
-    
-    formulario.appendChild(document.createElement("br"));
+
+    formulario.appendChild(br);
     formulario.appendChild(document.createTextNode("Numero celular: "));
     formulario.appendChild(numeroCelularInput);
-    
-    formulario.appendChild(document.createElement("br"));
 
-    // Botón para enviar el formulario
-    var enviarButton = document.createElement("input");
+
+    const listaProductosDiv = document.createElement("div");
+
+    for (const numero in productosSeleccionados) {
+        const producto = productosSeleccionados[numero];
+        const productoDiv = document.createElement("div");
+   
+        listaProductosDiv.appendChild(productoDiv);
+    }
+
+    formulario.appendChild(listaProductosDiv);
+
+    formulario.appendChild(br);
+
+    const enviarButton = document.createElement("input");
     enviarButton.type = "submit";
     enviarButton.value = "Enviar";
     formulario.appendChild(enviarButton);
 }
 
-selectButtons.forEach(function(button, index) {
-    button.addEventListener("click", function() {
-        var valor = contadores[index].value;
+selectButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        const valor = contadores[index].value;
         if (valor !== "") {
-            agregarProducto(index + 1, valor);
+            const botonSeleccionado = selectButtons[index];
+            const nombreProducto = botonSeleccionado.getAttribute("data-product");
+            agregarProducto(index + 1, valor, nombreProducto);
         }
     });
 });
 
-for (var i = 0; i < sumadores.length; i++) {
-    sumadores[i].addEventListener("click", function(event) {
-        var index = Array.from(sumadores).indexOf(event.target);
-        contadores[index].value = Number(contadores[index].value) + 1;
+sumadores.forEach((sumador, index) => {
+    sumador.addEventListener("click", () => {
+        contadores[index].value = String(Number(contadores[index].value) + 1);
         calcular(index);
     });
-}
+});
 
-for (var i = 0; i < restadores.length; i++) {
-    restadores[i].addEventListener("click", function(event) {
-        var index = Array.from(restadores).indexOf(event.target);
-        contadores[index].value = Number(contadores[index].value) - 1;
+restadores.forEach((restador, index) => {
+    restador.addEventListener("click", () => {
+        contadores[index].value = String(Number(contadores[index].value) - 1);
         calcular(index);
     });
-}
+});
 
-for (var i = 0; i < contadores.length; i++) {
-    contadores[i].addEventListener("change", function(event) {
-        var index = Array.from(contadores).indexOf(event.target);
-        calcular(index);
-    });
+contadores.forEach((contador, index) => {
+    contador.addEventListener("change", () => calcular(index));
 
-    contadores[i].addEventListener("keyup", function(event) {
-        var index = Array.from(contadores).indexOf(event.target);
+    contador.addEventListener("keyup", (event) => {
         if (contadores[index].value === "") {
             return;
         }
         calcular(index);
     });
-}
-
+});
