516295cf931e5f4ea016334932814e2c01a9c19f
Aquiles Piña Olvera <87087900+Aquilespina@users.noreply.github.com>
2023-10-28 14:28:38 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,5 @@
 <?php
-// Comprobar si se ha enviado el formulario
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Datos del formulario
-    $nombre = $_POST["nombre"];
-    $correo = $_POST["correo"];
-    $productosSeleccionados = $_POST["productosSeleccionados"]; // Este es un arreglo con la información de productos y valores
 
-    // Conexión a la base de datos (configura estos valores)
-    $servername = "tu_servidor";
-    $username = "tu_usuario";
-    $password = "tu_contraseña";
-    $dbname = "tu_base_de_datos";
-
-<<<<<<< HEAD
     $servername = "localhost";
  $username = "id21388328_adm";
  $password = "Producto01#";
@@ -54,26 +41,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 $conn->close();
-=======
-    // Crear una conexión a la base de datos (puedes usar MySQLi o PDO)
-    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Comprobar la conexión
-    if ($conn->connect_error) {
-        die("Error en la conexión a la base de datos: " . $conn->connect_error);
-    }
-
-    // Insertar datos del formulario en la tabla de pedidos
-    $sql = "INSERT INTO pedidos (nombre, correo, productos) VALUES ('$nombre', '$correo', '$productosSeleccionados')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "El pedido se ha registrado con éxito.";
-    } else {
-        echo "Error al registrar el pedido: " . $conn->error;
-    }
-
-    // Cerrar la conexión a la base de datos
-    $conn->close();
-}
-?>
->>>>>>> 753124a511db699726fe872a42a496abe2955034
